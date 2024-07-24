7d262afd935046285e458702e5318410efb0f3ae
SiddharthRajendran <siddharthrajendran582@gmail.com>
2024-07-24 19:44:45 +0530

    updating eks key for s3 bucket

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tetsdatabuck" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "eks/terraform.tfstate"
     region = "ap-south-1"
   }
 }
